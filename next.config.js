fec6a2b848651848fdd481549cecdd52c9c1c6e9
landerson98n <landerson98n@gmail.com>
2025-07-30 12:08:05 -0300

    stripe

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        domains: ["placeholder.svg"],
+    },
+}
